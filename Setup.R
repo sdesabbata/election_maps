f94f7a62ab0e145983c553389a09d7604ead7c61
sdesabbata <s.desabbata@le.ac.uk>
2019-12-18 08:31:42 +0000

    Minor fix

--- Setup.R
+++ Setup.R
@@ -38,3 +38,6 @@ drive_download(
 
 # Create dots dir if necessary
 ifelse(!dir.exists(file.path(".", "dots")), dir.create(file.path(".", "dots")), FALSE)
+
+# Create maps dir if necessary
+ifelse(!dir.exists(file.path(".", "maps")), dir.create(file.path(".", "maps")), FALSE)
